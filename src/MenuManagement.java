af25e28e827769abe1ac33fcb66d282ea7c26d3e
songjangmi <dhdlqmdlekd12@naver.com>
2021-03-16 01:41:06 +0900

    added MenuManager.java

---
+++ src/MenuManagement.java
@@ -0,0 +1,11 @@
+public class MenuManagement {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Subjects");
+		System.out.println("2. Delete Subjects");
+		System.out.println("3. Edit Subjects");
+		System.out.println("4. View Subjects");
+		System.out.println("5. Exit");
+	}
+
+}
