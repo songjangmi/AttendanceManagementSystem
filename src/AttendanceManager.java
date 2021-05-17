09c99484fd7e6ed7c878936a41cef6c672d05504
songjangmi <dhdlqmdlekd12@naver.com>
2021-05-17 23:32:24 +0900

    AttendanceManager

--- src/AttendanceManager.java
+++ src/AttendanceManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import attendance.Attendance;
@@ -18,7 +19,9 @@ public class AttendanceManager {
 	public void addStudent() {
 		int kind=0;
 		AttendanceInput attendanceInput;
-		while(kind!=1 && kind !=2) {
+		while(kind<=1 || kind >3) {
+			try {
+		System.out.print("go into add attendances in while");
 		System.out.print("1 for University");
 		System.out.print("2 for Fitness");
 		System.out.print("3 for Personal Training");
@@ -31,14 +34,14 @@ public class AttendanceManager {
 			break;
 		}
 		else if (kind==2) {
-			attendanceInput= new FitnessAttendance(AttendanceKind.Fitness);
+			attendanceInput= (AttendanceInput) new FitnessAttendance(AttendanceKind.Fitness);
 			attendanceInput.getUserInput(input);
 			attendances.add(attendanceInput);
 			break;
 		}
 		
 		else if (kind==3) {
-			attendanceInput= new PersonalTrainingAttendance(AttendanceKind.PersonalTraining);
+			attendanceInput= (AttendanceInput) new PersonalTrainingAttendance(AttendanceKind.PersonalTraining);
 			attendanceInput.getUserInput(input);
 			attendances.add(attendanceInput);
 			break;
@@ -47,12 +50,26 @@ public class AttendanceManager {
 		else {
 			System.out.print("Select num for Attendance Kind between 1 and 2:");
 		}
-		}		
+		}	
+		catch(InputMismatchException e) {
+			System.out.println("Please put an integer between 1 and 3!");
+			if(input.hasNext()) {
+				input.next();
+			}
+			kind=-1;
+			
+		}
+	}
 	}
 	
 	public void deleteSubjects() {
 		System.out.println("subject Name:");
 		String subjectName=input.next();
+		int index=findIndex(subjectName);
+		removefromSubjects(index, subjectName);
+	}
+	
+	public int findIndex(String subjectName) {
 		int index=-1;
 		for(int i=0; i<attendances.size(); i++) {
 			if(attendances.get(i).getSubjectName()==subjectName) {
@@ -60,63 +77,51 @@ public class AttendanceManager {
 				break;
 			}
 		}
+		return index;
+	}
+		
+	
+	public int removefromSubjects(int index, String subjectName) {
 		if  (index>=0) {
 			attendances.remove(index);
 			System.out.println("the student "+ subjectName + "is deleted");
+			return 1;
 		}
 		else {
 			System.out.println("the subject has not been registered");
-			return;
+			return -1;
 		}
 		
 	}
+	
 	
 	public void editSubjects() {
 		System.out.print("subject Name:");
 		String subjectName=input.next();
 		for(int i=0; i<attendances.size(); i++) {
-			AttendanceInput attendanceInput=attendances.get(i);
-			if(attendanceInput.getSubjectName()==subjectName) {
+			AttendanceInput attendance=attendances.get(i);
+			if(attendance.getSubjectName()==subjectName) {
 				int num=-1;		
 				while(num !=5) {
-					System.out.println("**Subject ivfo Edit Menu**");
-					System.out.println("1. Add Subjects");
-					System.out.println("2. Delete Subjects");
-					System.out.println("3. Edit Subjects");
-					System.out.println("4. View Subjects");
-					System.out.println("5. Exit");
-					System.out.println("Select one number between1-6:");
+					showEditSubjects();
 					num=input.nextInt();
-					if(num==1) {
-						System.out.print("subject Name:");
-						String SubjectName=input.next();
-						attendanceInput.setSubjectName(subjectName);
-					}
-			
-					else if(num==2) {
-						System.out.print("subject day/time):");
-						String subjectTime=input.next();
-						attendanceInput.setSubjectTime(subjectTime);
-				
-					}
-			
-					else if(num==3) {
-						System.out.print("professor Name:");
-						String professorName=input.next();
-						attendanceInput.setProfessorName(professorName);
+					switch(num) {
+					case 1:
+						attendance.setSubjectName( input);
+						break;
+					case 2:	
+						attendance.setSubjectTime( input);
+						break;
 						
-					}
-			
-					else if(num==4) {
-						System.out.print("professor Phone Number:");
-						String phone=input.next();
-						attendanceInput.setPhone(phone);
-					}
-			
-					else  {
-						continue;
-					}
-			
+					case 3:
+						attendance.setprofessorName( input);
+						break;
+					case 4:	
+						attendance.setPhone(input);
+						break;
+						default:
+							continue;					
+					}			
 				}
 				break;
 					
@@ -125,14 +130,22 @@ public class AttendanceManager {
 	}
 	
 	public void viewSubjects() {
-//		System.out.println("subject Name:");
-//		String subjectName=input.next();
 		System.out.println("# of registered attendances:" + attendances.size());
 		for(int i=0; i<attendances.size(); i++) {
-			attendances.get(i).printInfo();
-			
+			attendances.get(i).printInfo();			
 		}
 		}
+
+	
+	public void showEditSubjects() {	
+		System.out.println("**Subject ivfo Edit Menu**");
+		System.out.println("1. Add Subjects");
+		System.out.println("2. Delete Subjects");
+		System.out.println("3. Edit Subjects");
+		System.out.println("4. View Subjects");
+		System.out.println("5. Exit");
+		System.out.println("Select one number between1-6:");
 		
+	}
 }
 
