d8e19eeb94adb2ed37398ff55ff845637d0e59eb
songjangmi <dhdlqmdlekd12@naver.com>
2021-05-17 21:49:19 +0900

    MenuManager

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,41 +1,57 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class MenuManagement {
+public class MenuManager {
 	public static void main(String[] args) {
 		Scanner input=new Scanner(System.in);
 		AttendanceManager attendanceManager=new AttendanceManager(input);
-		
+		selectMenu(input, attendanceManager);
+			
+			}
+	public static void selectMenu(Scanner input, AttendanceManager attendanceManager ) {
 		int num=-1;		
 		while(num !=5) {
+			try {
+			showMenu();
+			num=input.nextInt();
+			switch(num) {
+			case 1:
+				attendanceManager.addStudent();
+				break;
+			case 2:
+				attendanceManager.deleteSubjects();
+				break;
+			case 3:
+				attendanceManager.editSubjects();
+				break;
+			case 4:
+				attendanceManager.viewSubjects();
+				break;
+			default:
+					continue;
+			}			
+			}
+			catch(InputMismatchException e) {
+				System.out.println("Please put an integer between 1 and 5!");
+				if(input.hasNext()) {
+					input.next();
+				}
+				num=-1;
+		
+				
+			}
+	}
+	}
+	
+	public static void showMenu() {
 		System.out.println("1. Add Subjects");
 		System.out.println("2. Delete Subjects");
 		System.out.println("3. Edit Subjects");
 		System.out.println("4. View Subjects");
 		System.out.println("5. Exit");
-		System.out.println("Select one number between1-6:");
-		num=input.nextInt();
-		if(num==1) {
-			attendanceManager.addStudent();
-		}
-		
-		else if(num==2) {
-			attendanceManager.deleteSubjects();
-		}
-		
-		else if(num==3) {
-			attendanceManager.editSubjects();
-		}
-		
-		else if(num==4) {
-			attendanceManager.viewSubjects();
-		}
-		
-		else  {
-			continue;
-		}
-		
-		}
+		System.out.println("Select one number between1-6:");	
 	}
+
 	public static void addStudent() {
 		Scanner input=new Scanner(System.in);
 		System.out.print("subject Name:");
